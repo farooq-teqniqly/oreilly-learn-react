d7f52c5913ec8f9e0b1d18d0fe5f7eaf12cb1ba3
farooq-teqniqly <farooq@teqniqly.com>
2023-12-28 13:09:12 -0800

    SVG icons

--- myapp/src/Alert.tsx
+++ myapp/src/Alert.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { ReactComponent as CrossIcon } from "./cross.svg";
+import { ReactComponent as InfoIcon } from "./info.svg";
+import { ReactComponent as WarningIcon } from "./warning.svg";
 
 type Props = {
   type?: "information" | "warning";
@@ -27,11 +30,15 @@ export function Alert({ type = "information", heading, children, closable, onClo
     >
       <div className="flex items-center mb-1">
         <span
-          className="w-7"
+          className="inline-block w-7"
           role="img"
           aria-label={type === "warning" ? "Warning" : "Information"}
         >
-          {type === "warning" ? "⚠" : "ℹ️"}
+          {type === "warning" ? (
+            <WarningIcon className="fill-amber-900 w-5 h-5"></WarningIcon>
+          ) : (
+            <InfoIcon className="fill-teal-900 w-5 h-5"></InfoIcon>
+          )}
         </span>
         <span className="font-bold">{heading}</span>
         {closable && (
@@ -40,9 +47,7 @@ export function Alert({ type = "information", heading, children, closable, onClo
             aria-label="Close"
             onClick={handleCloseClick}
           >
-            <span role="img" aria-label="Close">
-              ❌
-            </span>
+            <CrossIcon></CrossIcon>
           </button>
         )}
       </div>
