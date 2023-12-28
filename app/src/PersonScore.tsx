55294133c19487355c211d666af7434d269a64bb
farooq-teqniqly <farooq@teqniqly.com>
2023-12-27 19:30:58 -0800

    useState example

--- app/src/PersonScore.tsx
+++ app/src/PersonScore.tsx
@@ -1,14 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getPerson } from "./getPerson";
 
 export function PersonScore() {
+  const [name, setName] = useState<string | undefined>();
+  const [score, setScore] = useState(0);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     async function fetchData() {
       const person = await getPerson();
-      console.log(person);
+      setLoading(false);
+      setName(person.name);
     }
     fetchData();
   }, []);
 
-  return null;
+  if (loading) {
+    return <div>Fetching data...</div>;
+  }
+
+  return (
+    <div>
+      <h3>
+        {name}, {score}
+      </h3>
+      <button onClick={() => setScore((s) => s + 1)}>Add</button>
+      <button onClick={() => setScore((s) => s - 1)}>Subtract</button>
+      <button onClick={() => setScore(0)}>Reset</button>
+    </div>
+  );
 }
