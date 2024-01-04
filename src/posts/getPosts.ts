3f269927c6349bfb8cbd02b7c7a098d340a6be86
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-01-04 13:31:23 -0800

    Strongly typed response from API

---
+++ src/posts/getPosts.ts
@@ -0,0 +1,44 @@
+import { PostData } from "./types";
+
+export async function getPosts() {
+  const response = await fetch(process.env.REACT_APP_API_URL!);
+  const body = (await response.json()) as unknown;
+  assertIsPosts(body);
+  return body;
+}
+
+export function assertIsPosts(postsData: unknown): asserts postsData is PostData[] {
+  if (!Array.isArray(postsData)) {
+    throw new Error("posts is not an array");
+  }
+
+  if (postsData.length === 0) {
+    return;
+  }
+
+  postsData.forEach((p) => {
+    if (!("id" in p)) {
+      throw new Error("post doesn't contain an id");
+    }
+
+    if (typeof p.id !== "number") {
+      throw new Error("id is not a number");
+    }
+
+    if (!("title" in p)) {
+      throw new Error("post doesn't contain a title");
+    }
+
+    if (typeof p.title !== "string") {
+      throw new Error("title is not a string");
+    }
+
+    if (!("description" in p)) {
+      throw new Error("post doesn't contain a description");
+    }
+
+    if (typeof p.description !== "string") {
+      throw new Error("description is not a string");
+    }
+  });
+}
