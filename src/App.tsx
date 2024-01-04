971192a2505eeb8f55419782c7ad7dd14ee15071
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-01-04 13:15:56 -0800

    Initial commit of chapter 9 app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div className={`container mx-auto`}>
+      <div className={`flex flex-col p-10 text-center text-xl`}>
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
+}
+
+export default App;
