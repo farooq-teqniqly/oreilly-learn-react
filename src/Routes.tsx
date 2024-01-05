1fe1ae91f99dac7494e8e417883c325f7a737cb1
farooq-teqniqly <farooq@teqniqly.com>
2024-01-05 15:34:35 -0800

    Get and save posts using React Query

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter, RouterProvider, defer } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import NotFoundErrorPage from "./pages/NotFoundErrorPage";
 import HomePage from "./pages/HomePage";
 import PostsPage from "./posts/PostsPage";
 import { getPosts } from "./posts/getPosts";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -28,5 +31,9 @@ const router = createBrowserRouter([
 ]);
 
 export default function Routes() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
+  );
 }
--- src/posts/PostsPage.tsx
+++ src/posts/PostsPage.tsx
@@ -1,32 +1,56 @@
 import React from "react";
 import { Suspense } from "react";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useLoaderData, Await } from "react-router-dom";
-import { assertIsPosts } from "./getPosts";
+import { assertIsPosts, getPosts } from "./getPosts";
 import { assertIsData, savePost } from "./savePost";
-import { PostData, NewPostData } from "./types";
+import { PostData } from "./types";
 import PostLists from "./PostLists";
 import NewPostForm from "./NewPostForm";
 
 function PostsPage() {
-  const data = useLoaderData();
-  assertIsData(data);
+  // const data = useLoaderData();
+  // assertIsData(data);
 
-  async function handleSave(newPostData: NewPostData) {
-    await savePost(newPostData);
+  const {
+    isLoading,
+    isFetching,
+    data: posts,
+  } = useQuery({ queryKey: ["postsData"], queryFn: getPosts });
+
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: savePost,
+    onSuccess: (savedPost) => {
+      queryClient.setQueryData<PostData[]>(["postsData"], (oldPosts) => {
+        if (oldPosts === undefined) {
+          return [savedPost];
+        } else {
+          return [savedPost, ...oldPosts];
+        }
+      });
+    },
+  });
+
+  if (isLoading || posts === undefined) {
+    return <div className="mx-auto">Fetching posts...</div>;
   }
 
   return (
     <div className="mx-auto w-96">
       <h2 className="text-4xl text-slate-900 font-bold">Posts</h2>
-      <NewPostForm onSave={handleSave}></NewPostForm>
-      <Suspense fallback={<div>Fetching posts...</div>}>
+      <NewPostForm onSave={mutate}></NewPostForm>
+      <PostLists posts={posts}></PostLists>
+      {/* {isFetching ? <div>Refreshing list of posts...</div> : <PostLists posts={posts}></PostLists>} */}
+      {/* <Suspense fallback={<div>Fetching posts...</div>}>
         <Await resolve={data.posts}>
           {(p) => {
             assertIsPosts(p);
             return <PostLists posts={p}></PostLists>;
           }}
         </Await>
-      </Suspense>
+      </Suspense> */}
     </div>
   );
 }
